924a3e11463680b1f8be8763880595a71edf8bb5
Ndiaye <diabir_n@etna-alternance.net>
2022-01-06 17:44:54 +0100

    Added lombok to replace getters, setters, toString, and constructor in CreateUserDto

--- src/main/java/com/ndiaye/api/dto/CreateUserDto.java
+++ src/main/java/com/ndiaye/api/dto/CreateUserDto.java
@@ -1,8 +1,15 @@
 package com.ndiaye.api.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class CreateUserDto {
 
     @NotEmpty(message = "Should not be empty")
@@ -15,87 +22,4 @@ public class CreateUserDto {
     private String zipCode;
     private String country;
     private Long age;
-
-    public CreateUserDto() {
-    }
-
-    public CreateUserDto(String firstname, String lastname, String email, String address, String zipCode,
-                         String country, Long age) {
-        this.firstname = firstname;
-        this.lastname = lastname;
-        this.email = email;
-        this.address = address;
-        this.zipCode = zipCode;
-        this.country = country;
-        this.age = age;
-    }
-
-    public String getFirstname() {
-        return firstname;
-    }
-
-    public void setFirstname(String firstname) {
-        this.firstname = firstname;
-    }
-
-    public String getLastname() {
-        return lastname;
-    }
-
-    public void setLastname(String lastname) {
-        this.lastname = lastname;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getZipCode() {
-        return zipCode;
-    }
-
-    public void setZipCode(String zipCode) {
-        this.zipCode = zipCode;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-    public Long getAge() {
-        return age;
-    }
-
-    public void setAge(Long age) {
-        this.age = age;
-    }
-
-    @Override
-    public String toString() {
-        return "CreateUserDto{" +
-                "firstname='" + firstname + '\'' +
-                ", lastname='" + lastname + '\'' +
-                ", email='" + email + '\'' +
-                ", address='" + address + '\'' +
-                ", zipCode='" + zipCode + '\'' +
-                ", country='" + country + '\'' +
-                ", age=" + age +
-                '}';
-    }
 }
